cd00637be917b5fb13c78f66adaf9927d63d3491
Jonathan Huang <wisehuang@gmail.com>
2018-07-31 17:26:57 +0800

    print callback message

--- src/main/java/com/line/pay/chatbot/controller/CallbackController.java
+++ src/main/java/com/line/pay/chatbot/controller/CallbackController.java
@@ -13,22 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.ServletContextAware;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.util.Base64;
-import java.util.stream.Collectors;
 
 @Controller
 public class CallbackController implements ServletContextAware {
     private static Logger logger = LogManager.getLogger(CallbackController.class.getName());
 
     private static final String X_LINE_SIGNATURE = "X-Line-Signature";
-    private static final String HASH_ALGORITHM = "HmacSHA256";
 
     @Autowired
     private DialogFlowService dialogFlowService;
@@ -41,40 +34,17 @@ public class CallbackController implements ServletContextAware {
             var signature = request.getHeader(X_LINE_SIGNATURE);
             var body = ByteStreams.toByteArray(request.getInputStream());
 
-            if (!isSignatureValid(signature, body)) {
-                return new ResponseEntity(HttpStatus.BAD_REQUEST);
-            }
+            logger.info("body:" + body);
+
+//            if (!isSignatureValid(signature, body)) {
+//                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+//            }
+
         } catch (Exception e) {
             logger.error(e);
         }
 
         return new ResponseEntity(HttpStatus.OK);
-    }
-
-    private boolean isSignatureValid(String signature, byte[] body) {
-        var result = false;
-
-        try {
-            var sha256HMAC = Mac.getInstance(HASH_ALGORITHM);
-            SecretKeySpec secretKey = new SecretKeySpec(lineMessageService.getChannelSecret().getBytes(StandardCharsets.UTF_8), "HmacSHA256");
-            sha256HMAC.init(secretKey);
-
-            final byte[] headerSignature = Base64.getDecoder().decode(signature);
-            final byte[] bodySignature = sha256HMAC.doFinal(body);
-
-            logger.info("headerSignature = " + headerSignature);
-            logger.info("bodySignature = " + bodySignature);
-
-            if(MessageDigest.isEqual(headerSignature, bodySignature)) {
-                result = true;
-            } else {
-                result = false;
-            }
-        } catch (Exception e) {
-            logger.error(e);
-        } finally {
-            return result;
-        }
     }
 
     @Override
--- src/main/java/com/line/pay/chatbot/service/LineMessageService.java
+++ src/main/java/com/line/pay/chatbot/service/LineMessageService.java
@@ -5,9 +5,18 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
+
 @Service
 public class LineMessageService {
     private static Logger logger=LogManager.getLogger(LineMessageService.class.getName());
+
+
+    private static final String HASH_ALGORITHM = "HmacSHA256";
 
     @Value("${line.channel.id}")
     private String channelId;
@@ -17,5 +26,31 @@ public class LineMessageService {
 
     public String getChannelSecret() {
         return this.channelSecret;
+    }
+
+    public boolean isSignatureValid(String signature, byte[] body) {
+        var result = false;
+
+        try {
+            var sha256HMAC = Mac.getInstance(HASH_ALGORITHM);
+            SecretKeySpec secretKey = new SecretKeySpec(this.getChannelSecret().getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+            sha256HMAC.init(secretKey);
+
+            final byte[] headerSignature = Base64.getDecoder().decode(signature);
+            final byte[] bodySignature = sha256HMAC.doFinal(body);
+
+            logger.info("headerSignature = " + headerSignature);
+            logger.info("bodySignature = " + bodySignature);
+
+            if(MessageDigest.isEqual(headerSignature, bodySignature)) {
+                result = true;
+            } else {
+                result = false;
+            }
+        } catch (Exception e) {
+            logger.error(e);
+        } finally {
+            return result;
+        }
     }
 }
